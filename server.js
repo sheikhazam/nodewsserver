e94a928caa797adb3e9186a90148e934cc1ccc58
sheikhazam <asheikh@hollandcollege.com>
2023-08-19 13:01:57 -0600

    init

---
+++ server.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const https = require('https');
+const WebSocket = require('ws');
+
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+};
+
+const server = https.createServer(options, (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('WebSocket Secure server is running.');
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (socket) => {
+    console.log('Client connected over WSS');
+
+    socket.on('message', (message) => {
+        console.log(`Received: ${message}`);
+        socket.send(`Server received: ${message}`);
+    });
+
+    socket.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(8080, () => {
+    console.log('WebSocket Secure server is running on port 8080');
+});
